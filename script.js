8c767fbd8f0dbd42e524d581c1824c555bdea497
Stella Kim <62909702+nakim97@users.noreply.github.com>
2021-10-28 00:15:21 -0700

    inital commit

---
+++ script.js
@@ -0,0 +1,260 @@
+const api_key = 'ee04596001b19a1964b017c76bc5d185'
+
+// Genre: Action id: 28
+async function actionMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=28&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+actionMovies();
+
+// Genre: Adventure id:12
+async function adventureMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=12&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+adventureMovies();
+
+// Genre: Animation id:16
+async function mysteryMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=16&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+mysteryMovies();
+
+// Genre: Comedy id: 35
+async function comedyMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=35&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+comedyMovies();
+
+// Genre: Crime id:80
+async function crimeMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=80&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+crimeMovies();
+
+// Genre: Documentary id: 99
+async function documentaryMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=99&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+documentaryMovies();
+
+// Genre: Drama id:18
+async function dramaMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=18&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+dramaMovies();
+
+// Genre: Family id: 10751
+async function familyMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=10751&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+familyMovies();
+
+// Genre: Fantasy id: 14
+async function fantasyMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=14&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+fantasyMovies();
+
+// Genre: History id:36
+async function historyMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=36&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+historyMovies();
+
+// Genre: Horror id:27
+async function horrorMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=27&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+horrorMovies();
+
+// Genre: Music id:10402
+async function musicMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=10402&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+musicMovies();
+
+// Genre: Mystery id: 9648
+async function mysteryMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=9648&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+mysteryMovies();
+
+// Genre: Romance id:10749
+async function romanceMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=10749&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+romanceMovies();
+
+// Genre: Science Fiction id: 878
+async function sciencefictionMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=878&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+sciencefictionMovies();
+
+// Genre: TV Movie id: 10770
+async function tvMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=10770&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+tvMovies();
+
+// Genre: Thriller id:53
+async function thrillerMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=53&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+thrillerMovies();
+
+// Genre: War id:10752
+async function warMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=10752&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+warMovies();
+
+// Genre: Western id:37
+async function westernMovies(){
+    var page = 0;
+    for (var i = 0; i < 500; i++){
+        page ++;
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=37&page=${page}`);
+        const jsonResponse = await response.json();
+         // console.log(jsonResponse);
+    }
+}
+westernMovies();
+
+
+
+// Search Query for Actor/Actresses
+
+document.getElementById("searchBtn").addEventListener("click", function(e){
+    const query = document.getElementById("searchbar").value;
+    e.preventDefault();
+    async function search(){
+        var page = 0;
+        for (var i = 0; i < 2; i++){
+            page ++;
+            const response = await fetch(`https://api.themoviedb.org/3/search/person?api_key=${api_key}&query=${query}&page=${page}`);
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+
+            
+            const searchContainer = document.getElementById("data");
+            
+            const displayResults = jsonResponse.results.map(data => {
+                return data.known_for
+            })
+            searchContainer.innerHTML += JSON.stringify(displayResults);
+            console.log(displayResults)
+
+            
+        }
+    }
+    search();
+    
+})
