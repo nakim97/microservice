c8a6063b45805394a8f6cecdfe222dc57ea44ce4
Stella Kim <62909702+nakim97@users.noreply.github.com>
2021-11-03 00:30:56 -0700

    update port

--- index.js
+++ index.js
@@ -451,4 +451,4 @@ app.get('/search/movies/:title', async(req,res) => {
 
 })
 
-app.listen(3000, () => console.log(`Server running on port 3000`))
+app.listen(4000, () => console.log(`Server running on port 4000`))
